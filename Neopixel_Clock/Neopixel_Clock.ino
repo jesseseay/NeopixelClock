2554efa4e2d8673d9eb60041b7e6d00eb6f33c85
Jesse Seay <jesseseay@Jesse-Seay-MBP.local>
2017-04-03 23:16:23 -0500

    functional version

--- Neopixel_Clock/Neopixel_Clock.ino
+++ Neopixel_Clock/Neopixel_Clock.ino
@@ -1,15 +1,18 @@
 /* in terminal: git commit -am "message" [enter]
  *  commits to github
+ *  git commit
+ *  git push
  */
 
 
 
-}#include <Wire.h>
+#include <Wire.h>
 
 
 #include <Adafruit_NeoPixel.h>
 
-#define PIN 2    // Parameter 1 = number of pixels in strip
+#define PIN 2    
+// Parameter 1 = number of pixels in strip
 // Parameter 2 = pin number (most are valid)
 // Parameter 3 = pixel type flags, add together as needed:
 // NEO_KHZ800 800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
@@ -20,14 +23,16 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(4, PIN, NEO_GRB + NEO_KHZ800);
 
 
 uint32_t black = strip.Color(0, 0, 0); //0
-uint32_t brown = strip.Color(10, 4, 0); // 1
+uint32_t brown = strip.Color(100, 52, 19); // 1 pink
+//uint32_t brown = strip.Color(109, 83, 16); // 1
+
 uint32_t red = strip.Color(136, 0, 0); // 2
-uint32_t orange = strip.Color(136, 29, 0); // 3
-uint32_t yellow = strip.Color(108, 101, 0); // 4
-uint32_t green = strip.Color(0, 67, 0);// 5
+uint32_t orange = strip.Color(136, 35, 0); // 3
+uint32_t yellow = strip.Color(120, 90, 0); // 4
+uint32_t green = strip.Color(10, 100, 10);// 5
 uint32_t blue = strip.Color(0, 0, 67); // 6
-uint32_t violet = strip.Color(62, 0, 163); // 7
-uint32_t grey = strip.Color(36, 49, 48); //cyan  8-- change to gray later
+uint32_t violet = strip.Color(62, 0, 80); // 7
+uint32_t grey = strip.Color(36, 49, 48); //  8
 uint32_t white = strip.Color(128, 128, 128); // 9
 
 uint32_t colorcode[] = {black, brown, red, orange, yellow, green, blue, violet, grey, white};
